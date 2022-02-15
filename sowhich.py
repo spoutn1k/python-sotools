700fb00bc96e94888c9727cdb93c3b80e61eab5a
Frederick Deny <fdeny@uoregon.edu>
2022-02-15 13:19:51 -0800

    replaced walrus operator in sowhich.py

--- sowhich.py
+++ sowhich.py
@@ -8,8 +8,9 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Usage: %s <libmpi.so.x>" % sys.argv[0], file=sys.stderr)
         sys.exit(1)
-
-    if path := resolve(sys.argv[1]):
+    
+    path = resolve(sys.argv[1])
+    if path:
         print(path)
         sys.exit(0)
     sys.exit(1)
