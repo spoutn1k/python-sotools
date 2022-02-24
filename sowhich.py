69499c60b1dcee728f3796365e59cd487bdfa58b
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2022-02-24 13:24:11 -0800

    Added coverage information

--- sowhich.py
+++ sowhich.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Usage: %s <libmpi.so.x>" % sys.argv[0], file=sys.stderr)
         sys.exit(1)
-    
+
     path = resolve(sys.argv[1])
     if path:
         print(path)
