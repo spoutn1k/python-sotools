7623ae1ab468d02409ed6a62558f138efaa67a0f
Jean-Baptiste Skutnik <jskutnik@uoregon.edu>
2022-05-24 10:55:07 -0700

    Added tests

--- tests/test_dl_cache.py
+++ tests/test_dl_cache.py
@@ -1,5 +1,8 @@
 import unittest
 from pathlib import Path
+from sotools.dl_cache.structure import BinaryStruct
+from sotools.dl_cache.hwcaps import (CacheExtension, CacheExtensionSection,
+                                     CacheExtensionTag)
 from sotools.dl_cache.dl_cache import (_cache_type, _cache_libraries,
                                        _CacheType, _CacheHeader,
                                        _CacheHeaderNew, _CacheHeaderOld,
@@ -52,6 +55,10 @@ class DLCacheTest(unittest.TestCase):
                     _FileEntryOld):
             self.assertIn(cls.__name__, str(cls()))
 
+            for (attribute, value) in cls.__structure__:
+                if attribute is not None:
+                    self.assertIn(attribute, str(cls()))
+
     def test_deserialize_bad_header(self):
         with open(MODERN_CACHE, 'rb') as cache_file:
             cache_data = cache_file.read()
@@ -83,3 +90,78 @@ class DLCacheTest(unittest.TestCase):
 
     def test_wrapper(self):
         self.assertTrue(host_libraries())
+
+    def test_sizeof(self):
+
+        class TestByte(BinaryStruct):
+            __structure__ = [('member', 'uint8_t')]
+
+        class TestBytes(BinaryStruct):
+            __structure__ = [('member', 'uint8_t'), ('other', 'uint8_t')]
+
+        class TestInt(BinaryStruct):
+            __structure__ = [('member', 'uint32_t')]
+
+        class TestLong(BinaryStruct):
+            __structure__ = [('member', 'uint64_t')]
+
+        self.assertEqual(BinaryStruct.sizeof(TestByte), 1)
+        self.assertEqual(BinaryStruct.sizeof(TestBytes), 2)
+        self.assertEqual(BinaryStruct.sizeof(TestInt), 4)
+        self.assertEqual(BinaryStruct.sizeof(TestLong), 8)
+
+    def test_deserialize_header(self):
+        with open(MODERN_CACHE, 'rb') as cache_file:
+            cache_data = cache_file.read()
+
+        header = _CacheHeader.deserialize(cache_data)
+
+        self.assertTrue(header.nlibs)
+
+    def test_deserialize_entry(self):
+        with open(MODERN_CACHE, 'rb') as cache_file:
+            cache_data = cache_file.read()
+
+        header = _CacheHeader.deserialize(cache_data)
+        header_size = BinaryStruct.sizeof(header.__class__)
+        entry_type = header.__class__.entry_type
+
+        entry = entry_type.deserialize(cache_data[header_size:])
+        self.assertNotEqual(entry.key, 0)
+        self.assertNotEqual(entry.value, 0)
+        self.assertNotEqual(entry.flags, 0)
+
+        self.assertLess(entry.key, header.extension_offset)
+        self.assertLess(entry.value, header.extension_offset)
+
+    def test_deserialize_extension_header(self):
+        with open(MODERN_CACHE, 'rb') as cache_file:
+            cache_data = cache_file.read()
+
+        header = _CacheHeader.deserialize(cache_data)
+        extension_header = CacheExtension.deserialize(
+            cache_data[header.extension_offset:])
+
+        self.assertNotEqual(extension_header.count, 0)
+
+    def test_deserialize_extension_sections(self):
+        with open(MODERN_CACHE, 'rb') as cache_file:
+            cache_data = cache_file.read()
+
+        header = _CacheHeader.deserialize(cache_data)
+        extension_header = CacheExtension.deserialize(
+            cache_data[header.extension_offset:])
+        header_size = BinaryStruct.sizeof(CacheExtension)
+        section_size = BinaryStruct.sizeof(CacheExtensionSection)
+
+        for i in range(extension_header.count):
+            section = CacheExtensionSection.deserialize(
+                cache_data[header.extension_offset + header_size +
+                           i * section_size:])
+            self.assertIn(
+                section.tag, {
+                    CacheExtensionTag.TAG_GENERATOR,
+                    CacheExtensionTag.TAG_GLIBC_HWCAPS
+                })
+            self.assertNotEqual(section.offset, 0)
+            self.assertNotEqual(section.size, 0)
