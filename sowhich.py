1726aafda634e7cbebcac99c559bff910c6bcbdb
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2023-03-03 17:54:47 -0800

    Add proper argparsing to sowhich.py

--- sowhich.py
+++ sowhich.py
@@ -1,15 +1,41 @@
 #!/bin/env python3
 
 import sys
-
+import logging
+from argparse import ArgumentParser
 from sotools.linker import resolve
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Usage: %s <libmpi.so.x>" % sys.argv[0], file=sys.stderr)
-        sys.exit(1)
+DESCRIPTION = """This program will attempt to resolve an ELF file from a given shared object name. It allows to trace the attempts made by the linker to determine what shared object is resolved by what means."""
+EPILOG = """Please report any mismatch between the dynamic linker and the output of this program to http://github.com/spoutn1k/python-sotools."""
 
-    path = resolve(sys.argv[1])
+parser = ArgumentParser(
+    prog='sowhich',
+    description=DESCRIPTION,
+    epilog=EPILOG,
+)
+
+parser.add_argument(
+    "soname",
+    help="The library name to search for.",
+)
+
+parser.add_argument(
+    "-v",
+    "--verbose",
+    action="store_true",
+    help="Trace resolving attempts while searching for the library",
+)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="%(message)s",
+        )
+
+    path = resolve(args.soname)
     if path:
         print(path)
         sys.exit(0)
